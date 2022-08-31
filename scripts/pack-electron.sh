0331f88ab4ab8a41e378ae329f3deca56ee10f08
UniQMG <2382711-UniQMG@users.noreply.gitlab.com>
2022-08-31 21:07:01 +0000

    Update CI to build tpsecore, minor reorganization

--- scripts/pack-electron.sh
+++ scripts/pack-electron.sh
@@ -1,4 +1,5 @@
 # Before running: apt install p7zip-full
+# Call this script from the tetrio plus (..) directory
 set -e
 npm i -g asar
 
@@ -8,7 +9,7 @@ mv 'TETR.IO Setup v8.exe' 'TETR.IO Setup.exe'
 7z e app-64.7z "resources/app.asar" -y
 
 asar extract app.asar out
-node ./build-electron.js
+node ./scripts/build-electron.js
 cd out
 npm i node-fetch@2.6.1 whatwg-url xmldom image-size
 cd ..
