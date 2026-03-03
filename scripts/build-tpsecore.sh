147a3ec9c3eeb96ad8e92ce0964306d7522cc4af
WOSHIZHAZHA120 <WOSHIZHAZHA120@qq.com>
2026-03-04 03:08:05 +0800

    :sparkles: 合并上游

--- scripts/build-tpsecore.sh
+++ scripts/build-tpsecore.sh
@@ -1,17 +1,15 @@
 git pull --recurse-submodules # ensure tpsecore is pulled in
 
-# install rust, wasmpack, and related
+# install rust
 curl https://sh.rustup.rs -sSf | sh -s -- -y
-rustup default 1.64.0
 source "$HOME/.cargo/env"
-curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | bash
+rustup default 1.87.0
+rustup target add wasm32-unknown-unknown
 
 # build it
 git submodule init
 git submodule update
 cd tpsecore
-ls
-mkdir -p ../target
-CARGO_TARGET_DIR="../target" wasm-pack build --target web #--profile release
-cp pkg/tpsecore_bg.wasm pkg/tpsecore.js ../source/lib
+cargo build --profile release
+cp target/wasm32-unknown-unknown/release/tpsecore.wasm ../source/lib
 cd ..
